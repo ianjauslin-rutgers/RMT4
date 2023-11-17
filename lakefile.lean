f06d497021196e659aeaea7d5883b3c4cc9cb5a6
Ian Jauslin <ian.jauslin@rutgers.edu>
2023-11-17 17:08:38 -0500

    Fix lakefile

--- lakefile.lean
+++ lakefile.lean
@@ -7,7 +7,7 @@ package «rMT4» {
 
 require mathlib from git "https://github.com/leanprover-community/mathlib4.git"@"11572f182a36a4441f9a62246985ed9d2e1f3e32"
 
-require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4"@"main"
 
 @[default_target]
 lean_lib RMT4 {
